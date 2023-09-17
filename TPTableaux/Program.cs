abe3e5a4cd5b2262e273852aa24ef6095d77a430
Karim Ben Romdhane <karoumbr@gmail.com>
2023-09-17 11:00:48 +0100

    initial commit

---
+++ TPTableaux/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPTableaux
+{
+    public static class Program
+    {
+        public static int[] InsererTableau(int[] origine, int[] aInserer, int position)
+        {
+            int[] resultat = null;
+
+            return resultat;
+        }
+
+        public static void TransposerMatrice(double[,] matrice)
+        {
+
+        }
+
+        public static void RemplirRectangle(int[,] tableau, int x, int y, int largeur, int hauteur)
+        {
+            int lignes = tableau.GetLength(0);
+            int colonnes = tableau.GetLength(1);
+
+
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ UnitTestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright © HP 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c76b39be-6131-494e-88cc-e76402eb8364")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
